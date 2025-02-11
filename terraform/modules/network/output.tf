135efa9d6663d5ba5b87dacc413cfb0669738c64
YFanha <yannfnha@gmail.com>
2025-02-11 14:18:17 +0100

    feat: output variables

--- terraform/modules/network/output.tf
+++ terraform/modules/network/output.tf
@@ -1,3 +1,23 @@
 output "vpc_id" {
-  value       = aws_vpc.DMZ.id
+  value = aws_vpc.DMZ.id
+}
+
+output "dmz_subnet_id" {
+  value = aws_subnet.DMZ.id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private_subnet[*].id
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
+}
+
+output "dmz_route_table_id" {
+  value = aws_route_table.dmz_route_table.id
+}
+
+output "private_subnets_route_ids" {
+  value = aws_route_table.private_subnets_route[*].id
 }
