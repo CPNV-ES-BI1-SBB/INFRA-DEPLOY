3d00bc80c5cb7ff68b4a8bc05878b99ad8f7af95
YFanha <yannfnha@gmail.com>
2025-02-11 13:36:05 +0100

    feat: aws route creation

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -14,3 +14,34 @@ resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.DMZ.id
   cidr_block = var.private_subnets[count.index]["cidr_block"]
 }
+
+# Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.DMZ.id
+}
+
+# Routes for DMZ subnet
+resource "aws_route_table" "dmz_route_table" {
+  vpc_id = aws_vpc.DMZ.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+# Routes for private subnets
+resource "aws_route_table" "private_subnets_route" {
+  count = length(var.private_subnets)
+  vpc_id = aws_vpc.DMZ.id
+
+  route {
+    cidr_block = "10.0.0.0/16"
+    gateway_id = "local"
+  }
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "10.0.0.5/32"
+  }
+}
