79df865b78bb4bee91dfca2aa02df0a472ad8e7a
YFanha <yannfnha@gmail.com>
2025-02-25 18:04:20 +0100

    fix: correct ingress rule (use allowed_ips list)

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -105,12 +105,13 @@ resource "aws_security_group" "private_subnet_sg" {
 
 # Security group rules
 resource "aws_vpc_security_group_ingress_rule" "dmz_ingress_rules" {
+  count = length(var.allowed_ips)
   security_group_id = aws_security_group.dmz_subnet_sg.id
-  cidr_ipv4         = "193.5.240.9/32"
+  cidr_ipv4         = var.allowed_ips[count.index]
   from_port         = 22
   ip_protocol       = "tcp"
   to_port           = 22
-  description       = "SSH access from CPNV"
+  description       = "SSH access from allowed IPs"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "private_subnets_ingress_rules" {
